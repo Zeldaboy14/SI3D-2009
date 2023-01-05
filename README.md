df26bf159f6dc5d0761792b4c54fe43e435d2682
Zeldaboy14 <55566323+Zeldaboy14@users.noreply.github.com>
2023-01-05 05:06:39 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Sodor Island 3D 2009 (January 2012)
+![](https://github.com/Zeldaboy14/ZPW_2022/blob/main/docs/img/logo/zpw_logo_github.png?raw=true)
+
+A (nearly) complete archive of SI3D 2009 as it stood in January 2012. Features content archives too
+that I added in thanks to some anon's who assisted in file hunting. This WILL NOT host anything for
+SI3D 2010 or the early 2012 range as those are outside the scope (and probably not even allowed still)
+
---
+++ docs/css/logstate2-css.php
@@ -0,0 +1,23 @@
+.vloggedin { display: none; } .vloggedout { display: inline; }
+
+/*
+     FILE ARCHIVED ON 21:39:56 Jan 13, 2013 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 02:45:43 Dec 13, 2022.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 66.272
+  exclusion.robots: 0.109
+  exclusion.robots.policy: 0.099
+  RedisCDXSource: 0.661
+  esindex: 0.009
+  LoadShardBlock: 41.286 (3)
+  PetaboxLoader3.datanode: 75.071 (4)
+  CDXLines.iter: 21.198 (3)
+  load_resource: 75.518
+  PetaboxLoader3.resolve: 31.484
+*/
